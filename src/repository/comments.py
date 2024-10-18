9e26664ce27cf5d34fd715509db8d8843f0fe714
dmitriy <kylishovdimka@ukr.net>
2024-10-18 23:58:17 +0300

    Implemented profanity check for comments and posts

--- src/repository/comments.py
+++ src/repository/comments.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
@@ -20,6 +21,10 @@ async def get_comment_by_post(post_id: int, comment_id: int, db: AsyncSession):
 
 async def create_comment(post_id: int, body: CreateCommentSchema, db: AsyncSession):
     new_comment = Comment(post_id=post_id, **body.model_dump(exclude_unset=True))
+
+    if new_comment.check_profanity():
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Comment contains forbidden words")
+
     db.add(new_comment)
     await db.commit()
     await db.refresh(new_comment)
@@ -30,10 +35,18 @@ async def update_comment(comment_id: int, body: UpdateCommentSchema, db: AsyncSe
     stmt = select(Comment).filter_by(id=comment_id)
     result = await db.execute(stmt)
     comment = result.scalar_one_or_none()
-    if comment:
-        comment.description = body.description
-        await db.commit()
-        await db.refresh(comment)
+
+    if not comment:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Comment with id {comment_id} not found")
+
+    comment.description = body.description
+
+    if comment.check_profanity():
+        await db.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Comment contains forbidden words")
+
+    await db.commit()
+    await db.refresh(comment)
     return comment
 
 
